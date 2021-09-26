1bbb5f64091460aea6e73b29ee13d774951d9cb5
smoo <69005921+tmddn3070@users.noreply.github.com>
2021-09-26 11:26:08 +0900

    Create run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from discord_slash import SlashCommand
+from folder.config import *
+import os
+
+bot = commands.Bot(command_prefix='?')
+slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
+
+cogs = []
+path = "./smoobot/cogs"
+
+for filename in os.listdir(path):
+    if filename.endswith('.py'):
+        cogs.append(filename)
+        bot.load_extension(f'folder.cogs.{filename[:-3]}')
+    if filename == '__pycache__':pass
+
+print(f'{len(cogs)}개 스크립트 로드됨')
+
+bot.run(token)
